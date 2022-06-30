7f21072999f54f75595cddab03f2598c17b2cb87
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-06-30 13:55:36 -0500

    createing a not completely mediocore naviagtion bar

--- client/src/comp/Navbar.js
+++ client/src/comp/Navbar.js
@@ -1,16 +1,37 @@
 
 import React from 'react';
+import {Navbar, Container,Nav,NavDropdown} from 'react-bootstrap'
 
-
-function NavBar(){
+function topBar(){
 
 
     return(
+      <div>
 
-        <>
-        <p>The world is about</p>
-        </>
+<Navbar bg="light" expand="sm">
+  <Container>
+    <Navbar.Brand href="#home">Got Problems Write About it like the rest of them </Navbar.Brand>
+
+    <Navbar.Brand href="#home">Our Issue</Navbar.Brand>
+    
+    <Navbar.Brand href="#link">Common Issures </Navbar.Brand>
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+
+       
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.2">LogOut</NavDropdown.Item>
+           
+        </NavDropdown>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+
+      </div>
     )
 }
 
-export default NavBar
+export default topBar
