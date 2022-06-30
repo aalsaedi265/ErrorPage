42b259f831e9597482a6cbd64db321b8e269e766
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-06-30 13:14:54 -0500

    bootstrap, compoentes, and react began to communicate

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import {Button} from 'react-bootstrap';
 import './App.css';
+import NavBar from './comp/Navbar'
 
 function App() {
   return (
 
     <div className="App">
 
+    <NavBar/>
       <header className="App-header">
+
+      
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
 
-      <Button variant="success">Success</Button>{'the wets '}
+      
 
     
 
---
+++ client/src/comp/Navbar.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+
+function NavBar(){
+
+
+    return(
+
+        <>
+        <p>The world is about</p>
+        </>
+    )
+}
+
+export default NavBar
