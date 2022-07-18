4654d99d65cbbf4a588ae8c40e99055480ec06e8
Ahmed Alsaedi <aalsaedi265@gmail.com>
2022-07-18 15:14:49 -0500

    basic form

--- client/src/comp/Credientals.js
+++ client/src/comp/Credientals.js
@@ -8,7 +8,17 @@ export default function Credientals(){
 
         <>
             <header className="App-header">
+                <form>
 
+                    <p>Number</p>
+                    <input type='integer'name='id'/>
+                    <br></br>
+                    <p>Name</p>
+                    <input type='text' name='name'/>
+                    <br></br>
+                   
+                    <input style={{color:'black'}} type='button' value='submit'/>
+                </form>
       
             <img src={logo} className="App-logo" alt="logo" />
 
